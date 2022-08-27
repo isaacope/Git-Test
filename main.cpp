1f46eb1ead99960b6b2ddf927ad3e16b7b8184b8
Isaac Copeland <isaacc0628.ic@gmail.com>
2022-08-26 22:02:50 -0500

    first commit of main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Isaac Copeland
+ * @date 2022-08-26
+ * @brief a hello world program
+ * 
+ * This program prints the message "Hello World!".
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
