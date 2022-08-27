9c4b3a745ebdafeb8b7ec8fda0525db2dd83f28c
Isaac Copeland <isaacc0628.ic@gmail.com>
2022-08-26 22:26:54 -0500

    updated functionality of main.cpp

--- main.cpp
+++ main.cpp
@@ -12,8 +12,23 @@
 
 using namespace std;
 
+int sum(int n);
+
 int main() {
   cout << "Hello World!" << endl;
+
+  int n;
+  cout << "Enter a number n: ";
+  cin >> n;
+  cout << "The sum of 1 to n is " << sum(n) << endl;
   
   return 0;
+}
+
+int sum(int n) {
+  int sum = 0;
+  for(int i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
 }
